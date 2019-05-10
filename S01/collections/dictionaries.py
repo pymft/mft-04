1a607edc37f0c16e79b65e45a04411fd536e4b91
iamvee <naeini.v@gmail.com>
2019-05-10 12:49:31 +0330

    dictionaries

---
+++ S01/collections/dictionaries.py
@@ -0,0 +1,15 @@
+# [10, 20, 7, 99]
+# person = [123456789, "John", "Smith", 1984]
+
+person = {"id": 123456789, "name": "John", "lastname": "Smith"}
+print(person["lastname"])
+
+track = {"name": "la vie en rose",
+         "singer": "Edith Piaf",
+         "milliseconds": 12323453,
+         "language": "French"}
+
+
+print(track["singer"])
+
+print(type(track))
