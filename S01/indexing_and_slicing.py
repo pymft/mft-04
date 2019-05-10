8dc6366a915f4fce2ab98470e3767fc9b8b77385
iamvee <naeini.v@gmail.com>
2019-05-10 12:37:32 +0330

    indexing and slicing

---
+++ S01/indexing_and_slicing.py
@@ -0,0 +1,18 @@
+#       0      1     2     3      4       5     6  7  8  9  10
+lst = [1000, 1002, 1005, 10004, 10000, 2343434, 0, 2, 3, 4 ,5]
+
+# print(lst[-1:0:-1])
+
+lst[1]
+lst[1:7]
+# lst[1::2]
+# lst[::-1]
+
+
+text = "All work and no play makes Jack a dull boy!"
+word = text[4:9]
+print(word)
+
+something = "redrum"
+word_new = something[::-1]
+print(word_new)
