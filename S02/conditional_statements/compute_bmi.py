647f23f09c2cdd2ca8e69588a24abbb0d9ec46c8
iamvee <naeini.v@gmail.com>
2019-05-17 11:19:35 +0330

    calculate bmi - version 2

--- S02/conditional_statements/compute_bmi.py
+++ S02/conditional_statements/compute_bmi.py
@@ -1,3 +1,14 @@
-weight = 80
-height = 1.75
+weight = int(input("enter weight (kg) : "))
+height = 1.8
 
+bmi = weight / (height * height)
+
+print(bmi)
+bmi = 17
+
+if bmi <= 18:
+    print("under-weight")
+elif bmi >= 25:
+    print("Normal!")
+else:
+    print("over-weight")
---
+++ S02/conditional_statements/if_else.py
@@ -0,0 +1,10 @@
+num = 10000
+
+if num % 3 == 0:
+    print("baghimande mosavi 0 ast!")
+
+if num % 3 == 1:
+    print("baghimande = 1 ")
+else:
+    print("2")
+
