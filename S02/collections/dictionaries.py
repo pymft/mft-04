5255ef748d90c1049e1137224f6fb5e49387b1f2
iamvee <naeini.v@gmail.com>
2019-05-17 11:48:04 +0330

    dictionaries review

---
+++ S02/collections/dictionaries.py
@@ -0,0 +1,10 @@
+# track = [1, "famous blue raincoat", "Leonard Cohen", 1980]
+
+track = {"id": 1,  "year": 1980, "title": "famous blue raincoat", "singer": "Leonard Cohen"}
+print(track["title"])
+print(track)
+track["language"] = "English"
+track["title"] = track["title"].title()
+print(track)
+
+# track.
