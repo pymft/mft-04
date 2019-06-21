b570180e81b29aa1be65dafef84826fd417b3da1
iamvee <naeini.v@gmail.com>
2019-06-21 10:07:14 +0330

    question 2

--- S05/function_review/is_triangle.py
+++ S05/function_review/is_triangle.py
@@ -1,3 +1,11 @@
+
+def type_of_triangle(a, b, c):
+    # "متساوی الساقین"
+    # متساوی الاضلاع
+    # قائم الزاویه
+    # هیچ کدام
+    # مثلث نیست!
+
 def is_triangle(a, b, c):
     if a < b + c and b < a + c and c < a + b:
         return True
