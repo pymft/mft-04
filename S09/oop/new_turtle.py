ecd642eedc9f1acc0c5c6811cf37f7654373874d
iamvee <naeini.v@gmail.com>
2019-07-19 10:15:46 +0330

    turtle

--- S09/oop/new_turtle.py
+++ S09/oop/new_turtle.py
@@ -1,34 +1,45 @@
 import turtle
+import math
 
 
 print(type(turtle.Turtle))
+
 
 class NewTurtle(turtle.Turtle):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.speed(8)
+        self.__dude = None
 
-    #     self.buddy = None
-    #
-    # def set_buddy(self, other):
-    #     self.buddy = other
-    #
-    # def come_closer(self):
-    #     pass
+    @property
+    def dude(self):
+        return self.__dude
+
+    @dude.setter
+    def dude(self, value):
+        self.__dude = value
+        if self.dude.dude != self:
+            self.dude.dude = self
 
     def turn(self):
         for i in range(4):
             self.forward(100)
             self.left(90)
 
+    def come_closer(self):
+        x1, y1 = self.pos()
+        x2, y2 = self.__dude.pos()
+        dx = x2 - x1
+        dy = y2 - y1
+        t = math.atan2(dy, dx)
+        d = math.degrees(t)
+        self.setheading(d)
+        self.__dude.setheading(180 + d)
+
+
 
 nt1 = NewTurtle()
 nt2 = NewTurtle()
 
-# nt1.set_buddy(nt2)
-
-nt1.left(10)
-nt1.forward(10)
-
-print(nt1.pos())
+nt2.dude = nt1
 
