37062ec6b828057d83587d13f2ce63f3535ec128
iamvee <naeini.v@gmail.com>
2019-07-26 09:59:29 +0330

    templates

--- S10/web_application/app/app.py
+++ S10/web_application/app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask('my application')
 
 
 @app.route('/')
 def main_page():
-    return "<h1>Hello</h1>"
+    return render_template("index.html")
+
+@app.route('/dashboard')
+@app.route('/dashboard/<user>/<int:rep>')
+def dashboard_page(user="None", rep=1):
+    return render_template("dashboard.html",
+                           username=user, rep=rep)
+
 
 app.run(host="0.0.0.0")
